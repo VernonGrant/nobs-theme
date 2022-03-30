df4cdbb632a498748e4df36d639ccccdd4146814
Vernon Grant <vernon@ruppell.io>
2022-03-30 12:03:13 +0800

    Adds some gray shades

--- nobs-theme.el
+++ nobs-theme.el
@@ -24,6 +24,7 @@
 ;; A soft dark theme that's intended for HDR monitors. The theme uses a lot of
 ;; mat colors to help keep things from being overly bright on HDR displays.
 
+
 ;;; Code:
 
 (deftheme nobs)
@@ -32,90 +33,101 @@
 	  (bg_darker  "grey11")       ;; #1c1c1c
 	  (bg_lighter "grey17")       ;; #2b2b2b
 	  (fg         "grey58")       ;; #949494
+	  (fg_darker  "grey45")       ;; #737373
+	  (fg_lighter "grey75")       ;; #bfbfbf
 	  (fg_muted   "grey30")       ;; #333333
 	  (comment "LightSalmon4")    ;; #8b5742
 	  (cursor  "DarkGoldenrod3")  ;; #cd9b1d
 	  (keyword "DarkGoldenrod4")  ;; #8b6508
       (str     "LightGoldenrod4") ;; #8b8878
+	  (warning "tomato4")         ;; #8b3626
 	  )
 
   (custom-theme-set-faces
    'nobs
-   `(cursor                         ((t (:background ,cursor :foreground, fg))))
-   `(default                        ((t (:background ,bg :foreground ,fg))))
-   `(fill-column-indicator          ((t (:foreground ,fg))))
-   `(font-lock-builtin-face         ((t (:foreground ,fg))))
-   `(font-lock-comment-face         ((t (:foreground ,comment))))
-   `(font-lock-constant-face        ((t (:foreground ,keyword))))
-   `(font-lock-doc-face             ((t (:foreground ,comment))))
-   `(font-lock-function-name-face   ((t (:foreground ,fg))))
-   `(font-lock-keyword-face         ((t (:foreground ,keyword))))
-   `(font-lock-string-face          ((t (:foreground ,str))))
-   `(font-lock-type-face            ((t (:foreground ,keyword))))
-   `(font-lock-variable-name-face   ((t (:foreground ,fg))))
-   `(font-lock-warning-face         ((t (:foreground ,fg :weight bold ))))
-   `(fringe                         ((t (:background ,bg_darker :foreground ,fg))))
-   `(hl-line                        ((t (:background ,bg_darker))))
-   `(line-number                    ((t (:background ,bg_darker :foreground ,fg_muted))))
-   `(line-number-current-line       ((t (:background ,bg_lighter :foreground ,keyword))))
-   `(link                           ((t (:foreground ,keyword :underline t))))
-   `(minibuffer-prompt              ((t (:foreground ,keyword :bold t))))
-   `(mode-line                      ((t (:foreground ,fg :background ,bg_darker))))
-   `(mode-line-inactive             ((t (:foreground ,fg_muted :background ,bg_darker))))
-   `(region                         ((t (:background ,bg_darker))))
-   `(shadow                         ((t (:foreground ,comment))))
-   `(highlight                      ((t (:background ,bg_lighter :foreground ,cursor))))
-   `(vertical-border                ((t (:foreground ,bg_darker))))
+   `(cursor                            ((t (:background ,cursor :foreground, fg))))
+   `(default                           ((t (:background ,bg :foreground ,fg))))
+   `(fill-column-indicator             ((t (:foreground ,fg))))
+   `(font-lock-builtin-face            ((t (:foreground ,fg))))
+   `(font-lock-comment-face            ((t (:foreground ,comment))))
+   `(font-lock-comment-delimiter-face  ((t (:foreground ,comment))))
+   `(font-lock-constant-face           ((t (:foreground ,keyword))))
+   `(font-lock-doc-face                ((t (:foreground ,comment))))
+   `(font-lock-negation-char-face      ((t (:foreground ,fg))))
+   `(font-lock-function-name-face      ((t (:foreground ,fg))))
+   `(font-lock-keyword-face            ((t (:foreground ,keyword))))
+   `(font-lock-string-face             ((t (:foreground ,str))))
+   `(font-lock-type-face               ((t (:foreground ,keyword))))
+   `(font-lock-variable-name-face      ((t (:foreground ,fg_darker))))
+   `(font-lock-warning-face            ((t (:foreground ,fg :weight bold))))
+   `(fringe                            ((t (:background ,bg_darker :foreground ,fg))))
+   `(hl-line                           ((t (:background ,bg_darker))))
+   `(line-number                       ((t (:background ,bg_darker :foreground ,fg_muted))))
+   `(line-number-current-line          ((t (:background ,bg_lighter :foreground ,keyword))))
+   `(link                              ((t (:foreground ,keyword :underline t))))
+   `(minibuffer-prompt                 ((t (:foreground ,keyword :bold t))))
+   `(mode-line                         ((t (:foreground ,fg :background ,bg_darker))))
+   `(mode-line-inactive                ((t (:foreground ,fg_muted :background ,bg_darker))))
+   `(region                            ((t (:background ,bg_darker))))
+   `(shadow                            ((t (:foreground ,comment))))
+   `(highlight                         ((t (:background ,bg_lighter :foreground ,cursor))))
+   `(vertical-border                   ((t (:foreground ,bg_darker))))
+   `(error                             ((t (:background ,bg_lighter :foreground ,warning))))
+   ;; font-lock-preprocessor-face
+   ;; font-lock-regexp-grouping-backslash
+   ;; font-lock-regexp-grouping-construct
 
    ;; Parenthesis Mode:
-   `(show-paren-match               ((t (:background ,bg_lighter :foreground ,cursor))))
+   `(show-paren-match                  ((t (:background ,bg_lighter :foreground ,cursor))))
 
    ;; Completions
-   `(completions-common-part        ((t (:foreground ,keyword))))
+   `(completions-common-part           ((t (:foreground ,keyword))))
 
    ;; I Search:
-   `(isearch                        ((t (:background ,bg_lighter :foreground ,cursor))))
-   `(lazy-highlight                 ((t (:background ,cursor :foreground ,bg_darker))))
+   `(isearch                           ((t (:background ,bg_lighter :foreground ,cursor))))
+   `(lazy-highlight                    ((t (:background ,cursor :foreground ,bg_darker))))
 
    ;; Dired:
-   `(dired-header                   ((t (:foreground ,keyword))))
-   `(dired-directory                ((t (:foreground ,fg :bold t))))
+   `(dired-header                      ((t (:foreground ,keyword))))
+   `(dired-directory                   ((t (:foreground ,fg :bold t))))
+   `(dired-marked                      ((t (:background ,bg_lighter :foreground ,cursor))))
+   `(dired-flagged                     ((t (:background ,bg_lighter :foreground ,warning))))
 
    ;; Completions:
-   `(compilation-info               ((t (:foreground ,keyword))))
-   `(completions-first-difference   ((t (:foreground ,fg :weight bold))))
-   `(completions-annotations        ((t (:foreground ,fg_muted))))
+   `(compilation-info                  ((t (:foreground ,keyword))))
+   `(completions-first-difference      ((t (:foreground ,fg :weight bold))))
+   `(completions-annotations           ((t (:foreground ,fg_muted))))
 
    ;; Org Mode:
-   `(org-level-1                    ((t (:foreground ,keyword :bold t))))
-   `(org-level-2                    ((t (:foreground ,fg))))
-   `(org-level-3                    ((t (:foreground ,fg))))
-   `(org-todo                       ((t (:foreground ,fg :bold t))))
-   `(org-done                       ((t (:foreground ,fg_muted :bold t))))
-   `(org-agenda-structure           ((t (:foreground ,fg))))
-   `(org-agenda-date                ((t (:foreground ,fg))))
-   `(org-agenda-date-today          ((t (:foreground ,fg :bold t))))
-   `(org-agenda-done                ((t (:foreground ,fg_muted))))
-   `(org-scheduled                  ((t (:foreground ,fg))))
-   `(org-scheduled-today            ((t (:foreground ,fg :bold t))))
-   `(org-date                       ((t (:foreground ,keyword :underline t))))
-   `(org-drawer                     ((t (:foreground ,str))))
-   `(org-special-keyword            ((t (:foreground ,str))))
-
+   `(org-level-1                       ((t (:foreground ,keyword :bold t))))
+   `(org-level-2                       ((t (:foreground ,fg))))
+   `(org-level-3                       ((t (:foreground ,fg))))
+   `(org-todo                          ((t (:foreground ,fg :bold t))))
+   `(org-done                          ((t (:foreground ,fg_muted :bold t))))
+   `(org-agenda-structure              ((t (:foreground ,fg))))
+   `(org-agenda-date                   ((t (:foreground ,fg))))
+   `(org-agenda-date-today             ((t (:foreground ,fg :bold t))))
+   `(org-agenda-done                   ((t (:foreground ,fg_muted))))
+   `(org-scheduled                     ((t (:foreground ,fg))))
+   `(org-scheduled-today               ((t (:foreground ,fg :bold t))))
+   `(org-date                          ((t (:foreground ,keyword :underline t))))
+   `(org-drawer                        ((t (:foreground ,str))))
+   `(org-special-keyword               ((t (:foreground ,str))))
+   `(org-scheduled-previously          ((t (:foreground ,warning)))) ;; Change this to proper face color.
 
    ;; Web Mode:
-   `(web-mode-doctype-face          ((t (:foreground ,fg))))
-   `(web-mode-html-tag-bracket-face ((t (:foreground ,fg))))
-   `(web-mode-html-attr-name-face   ((t (:foreground ,fg))))
-   `(web-mode-html-tag-face         ((t (:foreground ,fg))))
+   `(web-mode-doctype-face             ((t (:foreground ,fg))))
+   `(web-mode-html-tag-bracket-face    ((t (:foreground ,fg))))
+   `(web-mode-html-attr-name-face      ((t (:foreground ,fg_darker))))
+   `(web-mode-html-tag-face            ((t (:foreground ,fg))))
 
    ;; Company Mode:
-   `(company-tooltip                ((t (:background ,bg_darker :foreground ,fg))))
-   `(company-tooltip-annotation     ((t (:foreground ,fg_muted))))
-   `(company-tooltip-common         ((t (:background ,bg_lighter :foreground ,cursor))))
-   `(company-tooltip-selection      ((t (:background ,bg_lighter :foreground ,cursor))))
-   `(company-scrollbar-bg           ((t (:background ,bg_lighter))))
-   `(company-scrollbar-fg           ((t (:background ,cursor))))
+   `(company-tooltip                   ((t (:background ,bg_darker :foreground ,fg))))
+   `(company-tooltip-annotation        ((t (:foreground ,fg_muted))))
+   `(company-tooltip-common            ((t (:background ,bg_lighter :foreground ,cursor))))
+   `(company-tooltip-selection         ((t (:background ,bg_lighter :foreground ,cursor))))
+   `(company-scrollbar-bg              ((t (:background ,bg_lighter))))
+   `(company-scrollbar-fg              ((t (:background ,cursor))))
     ;; company-tooltip-search
     ;; company-tooltip-search-selection
     ;; company-tooltip-mouse
@@ -126,7 +138,7 @@
     ;; company-box-candidate
 
    ;; LSP User Interface:
-   `(lsp-ui-doc-background         ((t (:background ,bg_darker))))
+   `(lsp-ui-doc-background            ((t (:background ,bg_darker))))
    ;; lsp-ui-doc-background
    ;; lsp-ui-peek-filename
    ;; lsp-ui-peek-header
